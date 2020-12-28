3cd3da93577795d1f948aec938297e4940af0e5d
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-12-28 18:08:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,4 +27,13 @@ class Student
       
     DB[:conn].execute(sql)
   end
+  
+  def self.save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
 end
